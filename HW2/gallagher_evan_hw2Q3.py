2b9d2666c97dab09a542839b4d54d4d7e498d2a0
utsadev <32254826+utsadev@users.noreply.github.com>
2017-09-25 22:39:57 -0500

    HW2 changes
    Working on homework 2

--- HW2/gallagher_evan_hw2Q3.py
+++ HW2/gallagher_evan_hw2Q3.py
@@ -1,10 +1,33 @@
 
+def get_valid_number(conT):
+    pass
+    #conLen = raw_input("Length in %s to convert: "%conT)
+
+def get_valid_unit(conF, conT):
+
+    accepted_units = {'m','M','ft','FT'}
+
+    conF = raw_input("Convert from units: ")
+    if conF not in accepted_units:
+        print "You have entered wrong units"
+        SystemExit()
+
+    conT = raw_input("Convert to units: ")
+    if conT not in accepted_units:
+        print "You have entered wrong units"
+        SystemExit()
+    
+    return conF, conT
+
+
 def main():
 
-    
-    conF = raw_input("Convert from units: ")
-    conT = raw_input("Convert to units: ")
-    conLen = raw_input("Length in %s to convert: "%conT)
+    conF = ""
+    conT = ""
+
+    get_valid_unit(conF, conT)
+    print conF, conT
+    #get_valid_number(conT)
    
     
 
