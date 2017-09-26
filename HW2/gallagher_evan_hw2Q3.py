478758578b038741a007ded8b68698a0e8eb7474
utsadev <32254826+utsadev@users.noreply.github.com>
2017-09-26 14:56:06 -0500

    Finished all hw
    finished all hw except the bonus questions. Just need to comment the code

--- HW2/gallagher_evan_hw2Q3.py
+++ HW2/gallagher_evan_hw2Q3.py
@@ -1,35 +1,78 @@
+def convert(conF, conT, conLen):
+    # 1 meter = 3.281ft
+    # 1 foot = .305
+    meterToFt = 3.281
+    ftToMeter = .305
+    conF = conF.lower()
+    conT = conT.lower()
+    if conF == 'm':
+        conversion = ftToMeter * conLen
+    elif conF == 'ft':
+        conversion = meterToFt * conLen
+
+    return conversion
+
 
 def get_valid_number(conT):
-    pass
-    #conLen = raw_input("Length in %s to convert: "%conT)
 
-def get_valid_unit(conF, conT):
+    while True:
+        conLen = raw_input("Length in %s to convert: " % conT)
+        if conLen == 'q':
+            print "Done"
+            quit()
+        else:
+            conLen = int(conLen)
 
-    accepted_units = {'m','M','ft','FT'}
+        if type(conLen) != int:
+            print "sorry please enter another number"
+        else:
+            break
 
-    conF = raw_input("Convert from units: ")
-    if conF not in accepted_units:
-        print "You have entered wrong units"
-        SystemExit()
+    return conLen
 
-    conT = raw_input("Convert to units: ")
-    if conT not in accepted_units:
-        print "You have entered wrong units"
-        SystemExit()
-    
-    return conF, conT
+
+def get_valid_unit_F(accepted_units):
+
+    while True:
+        conF = raw_input("Convert from units: ")
+        if conF == 'q':
+            print "Done"
+            quit()
+        if conF not in accepted_units:
+            print "Sorry don't know anything about %s unit" % conF
+        else:
+            break
+
+    return conF
+
+
+def get_valid_unit_T(accepted_units):  # , conT):
+
+    while True:
+        conT = raw_input("Convert to units: ")
+        if conT == 'q':
+            print "Done"
+            quit()
+        if conT not in accepted_units:
+            print "Sorry don't know anything about %s unit" % conT
+        else:
+            break
+    return conT
 
 
 def main():
 
-    conF = ""
-    conT = ""
+    accepted_units = {'m', 'M', 'ft', 'FT'}
 
-    get_valid_unit(conF, conT)
-    print conF, conT
-    #get_valid_number(conT)
-   
-    
+    while True:
+        print "\nPlease enter 'q' to quite program"
+        conF = get_valid_unit_F(accepted_units)
+        conT = get_valid_unit_T(accepted_units)
+        conLen = get_valid_number(conT)
+        print ""
+        conv = convert(conF, conT, conLen)
+        print "%s %s = %.3f %s" % (conLen, conT, conv, conF)
+        print "================================"
 
 
 if __name__ == "__main__":
